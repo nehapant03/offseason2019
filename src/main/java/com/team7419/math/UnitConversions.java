ed60814cc5a8b335b9fb0756c9ad9abee3f31d1f
Neha Pant <npant03@gmail.com>
2019-11-27 13:07:11 -0800

    add kT and kV values for given motorTypes

---
+++ src/main/java/com/team7419/math/UnitConversions.java
@@ -0,0 +1,8 @@
+package com.team7419.math;
+
+public class UnitConversions{
+
+    public static double rpmToRadPerSec(double rpm){
+        return rpm * 2 * Math.PI / 60;
+    }
+}
--- src/main/java/com/team7419/motors/DcMotor.java
+++ src/main/java/com/team7419/motors/DcMotor.java
@@ -1,27 +1,55 @@
 package com.team7419.motors;
 
+import com.team7419.math.*;
+
 public class DcMotor{
 
-    public DcMotor(MotorType motorType){
+    private double kT;
+    private double kV;
 
-        double kT;
-        double kV;
+    /**
+     * simulation motor based on 971's motor model
+     * essentially looking at motor as generator + resistor
+     * @param motorType cim, mini cim, 775 pro
+     */
+    public DcMotor(MotorType motorType){
         
+        /**
+         * sets the motor constants kT and kV based on motorType
+         */
         switch(motorType.value){
             /** cim */
             case 0:
-                kT = 0;
-                kV = 1;
+                kV = getkV(131, 5330, 2.7);
+                kT = getkT(2.41, 131);
             /** mini cim */
             case 1:
-                kT = 2;
-                kV = 12;
+                kV = getkV(89, 5840, 3.0);
+                kT = getkT(1.41, 89);
             /** 775 pro */
             case 2:
-                kT = 3;
-                kV = 15;
+                kV = getkV(134, 18730, 0.7);
+                kT = getkT(.71, 134);
         }
+    }
 
+    /**
+     * @param kPeakCurrent in Amps, current at stall torque
+     * @param kFreeSpeed in rpm, when there's no load on motor
+     * @param kFreeCurrent in Amps, current at free speed
+     * @return voltage constant of the motor for use in calculations
+     */
+    public double getkV(double kPeakCurrent, double kFreeSpeed, double kFreeCurrent){
+        double kInternalResistance = 12 / kPeakCurrent;
+        return (UnitConversions.rpmToRadPerSec(kFreeSpeed) + kInternalResistance * kFreeCurrent) / 12;
+    }
 
+    /**
+     * @param kStallTorque in N*m, when motor is stalled
+     * @param kStallCurrent in Amps, current at stall torque
+     * @return torque constant of the motor for use in calculations
+     */
+    public double getkT(double kStallTorque, double kStallCurrent){
+        return kStallTorque / kStallCurrent;
     }
 }
