8e222e0ea2acc0a51286f18037748a3ce5d455d3
Neha Pant <npant03@gmail.com>
2019-11-27 20:57:43 -0800

    add generic constructor, getkInternalResistance()

--- src/main/java/com/team7419/motors/DcMotor.java
+++ src/main/java/com/team7419/motors/DcMotor.java
@@ -3,14 +3,18 @@ package com.team7419.motors;
 import com.team7419.math.*;
 
 /**
-     * simulation motor based on 971's motor model
-     * essentially modelling motor as generator + resistor
-     */
+* simulation motor based on 971's motor model
+* essentially modelling motor as generator + resistor
+*/
 public class DcMotor{
 
     private double kT;
     private double kV;
+    private double kInternalResistance;
 
+    /**
+     * constructs a DcMotor based on a given type
+     */
     public DcMotor(MotorType motorType){
         
         /**
@@ -19,17 +23,37 @@ public class DcMotor{
         switch(motorType.value){
             /** cim */
             case 0:
-                kV = getkV(131, 5330, 2.7);
+                kInternalResistance = getkInternalResistance(131);
+                kV = getkV(kInternalResistance, 5330, 2.7);
                 kT = getkT(2.41, 131);
             /** mini cim */
             case 1:
-                kV = getkV(89, 5840, 3.0);
+                kInternalResistance = getkInternalResistance(89);
+                kV = getkV(kInternalResistance, 5840, 3.0);
                 kT = getkT(1.41, 89);
             /** 775 pro */
             case 2:
-                kV = getkV(134, 18730, 0.7);
+                kInternalResistance = getkInternalResistance(134);
+                kV = getkV(kInternalResistance, 18730, 0.7);
                 kT = getkT(.71, 134);
         }
+    }
+
+    /**
+     * pretty much only for use setting up a transmission but like eh
+     * @param kV can be computed using getkV
+     * @param kT can be computed using getkT
+     * @param kInternalResistance can be computed using getkInternalResistance
+     */
+    public DcMotor(double kV, double kT, double kInternalResistance){
+        this.kV = kV;
+        this.kInternalResistance = kInternalResistance;
+        this.kT = kT;
+    }
+
+
+    private double getkInternalResistance(double kPeakCurrent){
+        return 12 / kPeakCurrent;
     }
 
     /**
@@ -38,9 +62,8 @@ public class DcMotor{
      * @param kFreeCurrent in Amps, current at free speed
      * @return voltage constant of the motor for use in calculations
      */
-    public double getkV(double kPeakCurrent, double kFreeSpeed, double kFreeCurrent){
-        double kInternalResistance = 12 / kPeakCurrent;
-        return (UnitConversions.rpmToRadPerSec(kFreeSpeed) + kInternalResistance * kFreeCurrent) / 12;
+    private double getkV(double internalResistance, double freeSpeed, double freeCurrent){
+        return (UnitConversions.rpmToRadPerSec(freeSpeed) + internalResistance * freeCurrent) / 12;
     }
 
     /**
@@ -48,7 +71,7 @@ public class DcMotor{
      * @param kStallCurrent in Amps, current at stall torque
      * @return torque constant of the motor for use in calculations
      */
-    public double getkT(double kStallTorque, double kStallCurrent){
-        return kStallTorque / kStallCurrent;
+    private double getkT(double stallTorque, double stallCurrent){
+        return stallTorque / stallCurrent;
     }
 }
