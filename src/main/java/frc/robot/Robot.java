2ad8d87ccb76f213e96ca4ee5dba2a999928af5e
Neha Pant <npant03@gmail.com>
2019-12-04 14:49:11 -0800

    start zero command and well like i dont wanna go to class rn but no tengo opciones so thats real great

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.robot.subsystems.arm.HallEffectSub;
+import frc.robot.subsystems.arm.PotentiometerSub;
 import frc.robot.subsystems.drive.*;
  
 public class Robot extends TimedRobot {
@@ -13,6 +14,7 @@ public class Robot extends TimedRobot {
   public static ArcadeCommand arcade;
   public static TankCommand tank;
   public static HallEffectSub hallEffect;
+  public static PotentiometerSub potentiometer;
 
   @Override
   public void robotInit() {
@@ -21,6 +23,8 @@ public class Robot extends TimedRobot {
     arcade = new ArcadeCommand(oi.joystick, driveBase.leftSide, driveBase.rightSide, .4, .4);
     tank = new TankCommand(oi.joystick, driveBase.leftSide, driveBase.rightSide, .2, .4);
     hallEffect = new HallEffectSub();
+    potentiometer = new PotentiometerSub();
+
   }
 
   @Override
---
+++ src/main/java/frc/robot/subsystems/arm/ZeroCommand.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems.arm;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ZeroCommand extends Command{
+
+    @Override
+    public void initialize(){
+
+    }
+
+    @Override
+    public void execute(){
+
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+
+    @Override
+    public void end(){
+        
+    }
+
+    @Override
+    public void interrupted(){
+        end();
+    }
+}
